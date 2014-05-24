2067b8a722502f39daccab651cd0c30bad39774f
carlos.oliv@gmail.com <carlos.oliv@gmail.com@4dabc297-5433-b5ab-c360-c3789a54f0a5>
2014-05-24 19:55:09 +0000

    Coloquei o codigo do projeto, o pdf com a descricao do trabalho e o arquivo de planejamento...

---
+++ trunk/Domino/src/domino/Domino.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domino;
+
+import gui.jFrame;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Domino {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame jFrame = new jFrame();
+
+    }
+}
---
+++ trunk/Domino/src/domino/Jogador.java
@@ -0,0 +1,12 @@
+package domino;
+
+public class Jogador {
+    String nome;
+	
+    public Jogador (String nm) {
+	this.nome = nm;
+    }
+	
+    public static void jogar () {}
+
+}
