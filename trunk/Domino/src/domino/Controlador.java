aebd23683a3541472c28d56067f1d433e3a973df
carlos.oliv@gmail.com <carlos.oliv@gmail.com@4dabc297-5433-b5ab-c360-c3789a54f0a5>
2014-05-26 00:26:34 +0000

    Organizei um pouco as classes Controlador.java e Jogo.java

--- trunk/Domino/src/domino/Controlador.java
+++ trunk/Domino/src/domino/Controlador.java
@@ -47,41 +47,8 @@ public class Controlador {
                 jogador = new Jogador("Jogador"+i);
             jogo.adicionaJogador(jogador);
         }
-
-        // Criando e distribuindo as pecas
-        jogo.misturaPecas();
         
-        //Peca peca = jogo.pecasJogo.get(0);
-        //System.out.println("Peca atual: "+peca);
-  
-        // Distribuindo pecas entre os jogadores
-        Peca peca;
-        int x=0; // Controla o array de pecas
-        for (int i=0; i<njogadores; i++) {
-            for (int j=0; j<6; j++) { // 6 Pecas para cada jogador...
-                peca = jogo.pecasJogo.get(x); // Pegar uma peca da lista de pecas...
-                jogo.jogadores.get(i).recebePeca(peca); // Dar a peca para o jogador
-                x++;
-            }
-        }
-        for (int i=0; i<x; i++) {
-            System.out.println("Removendo peca "+i+": "+jogo.pecasJogo.get(0));
-            jogo.pecasJogo.remove(0); // Remover as pecas ja distribuidas da lista de pecas do jogo
-        }
-        
-        
-        for (int i=0; i<njogadores; i++) {
-            System.out.println ("\nMostrando pecas do jogador "+i+":");
-            jogo.jogadores.get(i).mostraPecas();
-            System.out.println("");
-        }
-        
-        // Mostrando as pecas que sobraram...
-        System.out.println("Pecas que sobraram:");
-        for (int i=0; i<jogo.pecasJogo.size(); i++) {
-            System.out.println("Peca "+i+": "+jogo.pecasJogo.get(i));
-        }
-
+        jogo.preparaJogo ();
         jogo.iniciar();
     }
 }
